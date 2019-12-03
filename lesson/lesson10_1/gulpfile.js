6cce95ce22a6da58983233527c746c899b14a974
hisagy69 <sinegamy11@gmail.com>
2019-12-03 18:49:12 +0300

    feat: add fuction rename minify-css file *.min.css

--- lesson/lesson10_1/gulpfile.js
+++ lesson/lesson10_1/gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const browserSync = require('browser-sync').create();
 const minifyCSS = require('gulp-minify-css');
+const rename = require("gulp-rename");
 
 gulp.task('browser-sync', function() {
     browserSync.init({
@@ -9,10 +10,12 @@ gulp.task('browser-sync', function() {
         }
     });
     gulp.watch("./*.html").on(change, browserSync.reload);
+    gulp.watch("./*.css").on(change, browserSync.reload);
 });
 
 gulp.task('minify-css', function() {
     return gulp.src('./*.css')
       .pipe(minifyCSS({keepBreaks:true}))
-      .pipe(gulp.dest('./out'))
+      .pipe(rename({suffix: ".min"}))
+      .pipe(gulp.dest('./min/'))/* указываем папку, куда отправляет минифицированный css */
   });
