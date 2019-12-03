f84976b45a90dc8566aedc3534dd33411bb99d39
hisagy69 <sinegamy11@gmail.com>
2019-12-03 10:45:38 +0300

    setting gulpfile minify css

---
+++ lesson/lesson10_1/gulpfile.js
@@ -0,0 +1,18 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync').create();
+const minifyCSS = require('gulp-minify-css');
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch("./*.html").on(change, browserSync.reload);
+});
+
+gulp.task('minify-css', function() {
+    return gulp.src('./*.css')
+      .pipe(minifyCSS({keepBreaks:true}))
+      .pipe(gulp.dest('./out'))
+  });
