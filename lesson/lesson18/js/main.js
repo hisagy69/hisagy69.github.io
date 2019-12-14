4ca31d01a63e293031379974f98609628f327ba6
hisagy69 <sinegamy11@gmail.com>
2019-12-14 11:03:36 +0300

    feat: add modal window

---
+++ lesson/lesson18/js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  //проверка загрузки дом
+  const modal = document.querySelector(".modal"); //берет класс модал
+  const modalBtn = document.querySelectorAll("[data-toggle=modal]");//берет атрибут дата тогл
+  const modalClose = document.querySelector(".modal__close");//берет класс модал клос
+  const toggleModal = () => {
+    modal.classList.toggle("modal_visible");
+  }
+
+  //перебирает все элементы переменной модалбтн
+  modalBtn.forEach(element => {
+    //перехватывает событие клик по элементу
+    element.addEventListener("click", toggleModal);
+  })
+  modalClose.addEventListener("click", toggleModal);//когда один элемент, перебор создавать не надо
+});
