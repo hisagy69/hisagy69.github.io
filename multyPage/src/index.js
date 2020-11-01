384153604928aa043624755e9bcabee56814d97e
Adam <sinegamy11@gmail.com>
2020-11-02 00:53:59 +0700

    add: section

--- multyPage/src/index.js
+++ multyPage/src/index.js
@@ -10,6 +10,7 @@ import './sass/grid.sass';
 import './sass/interface.sass';
 import './sass/templates/header.sass';
 import './sass/templates/hero.sass';
+import './sass/templates/advantages.sass';
 import './sass/templates/footer.sass';
 // import logo from './img/logo.png';
 // if (module.hot) {
